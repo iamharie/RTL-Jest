275dde9bfd1983f283928ac550a807301c6da7b0
Hariharan Mohan <haranhari350@gmail.com>
2025-02-20 23:27:36 -0500

    __mock__ and converted the tsconfig.json to a single source

--- jest.config.ts
+++ jest.config.ts
@@ -2,11 +2,12 @@ import type { Config } from "jest";
 
 const config: Config = {
   preset: "ts-jest",
-  testEnvironment: "jsdom",
+  testEnvironment: "jest-environment-jsdom",
   moduleNameMapper: {
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy", // Mock CSS imports
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.ts",
   },
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  setupFilesAfterEnv: ["<rootDir>/src/setupTest.ts"],
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
   },
