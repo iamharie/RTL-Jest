88b628d1c8206d4b2b6e45c9f020f39b03111586
Hariharan Mohan <haranhari350@gmail.com>
2025-03-26 20:42:19 -0500

    Styles added

--- src/components/Desktop.tsx
+++ src/components/Desktop.tsx
@@ -5,9 +5,8 @@ import { Box } from "@mui/material";
 const Desktop: React.FC = () => {
   return (
     <div>
-      <Box sx={{ padding: 2 }}>
-        <p>Desktop</p>
-        <DropdownFlyerComponent />
+      <Box sx={{ padding: 2, backgroundColor: "black", color: "white" }}>
+        <p>Hari</p>
       </Box>
     </div>
   );
